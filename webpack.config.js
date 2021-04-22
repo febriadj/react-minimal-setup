d60aa1007778e519a4703aa83ad4291be028954c
i'mfebri <iamfebriadji@gmail.com>
2021-04-22 23:29:24 +0700

    menambahkan script devserver pada konfigurasi webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,22 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 3000,
+    hot: true
+  }
 }
